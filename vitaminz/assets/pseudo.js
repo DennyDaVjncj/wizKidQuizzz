92f3afea6ef42ae30e0f477e31d3014688302ff7
Denny da Vjncj <daVjncjCode@gmail.com>
2020-10-04 14:56:22 -0400

    jotted down some ideas

--- vitaminz/assets/pseudo.js
+++ vitaminz/assets/pseudo.js
@@ -1,1 +1,6 @@
-//get busy with difficult poo
+//OVERVIEW:
+    //eventHandlers trigger html updates via DOM
+        //serparate functions to handle diff aspects of DOM updates
+
+    //build out hishScores.html
+    //get timer running & responding to user activity
