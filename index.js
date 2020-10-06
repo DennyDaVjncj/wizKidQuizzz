3d33e13c1201e86410d802b8ed1dd8c0dab26227
Denny da Vjncj <daVjncjCode@gmail.com>
2020-10-06 11:22:52 -0400

    linked index.js

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 //target DOM elements for manipulation
     //create variables to store DOM elements for targeting
     //use variables for functioning
+
+//trigger dom 
+
+//store questions inside array
+
+let WKQ=confirm("are you ready for the mental gymnastics this quiz puts one through?");
+let WKQ0=prompt("here's a primer: 'please define the nature of reality (all things being relative of course)");
