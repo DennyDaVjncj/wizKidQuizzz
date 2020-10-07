8d53b07f7567051fad5f0fc8ad6705e8243974cc
Denny da Vjncj <daVjncjCode@gmail.com>
2020-10-06 23:19:46 -0400

    developing functionality

--- index.js
+++ index.js
@@ -6,5 +6,10 @@
 
 //store questions inside array
 
-let WKQ=confirm("are you ready for the mental gymnastics this quiz puts one through?");
+let WKQ=confirm("are you ready for the mental gymnastics this quiz puts one through?");//I want to create a fun, enlightning experience
 let WKQ0=prompt("here's a primer: please define the nature of reality (all things being relative of course)");//if input is less than 4char, alert('you didn't even try though...');
+let prmpt1Fnctn=()=>{
+    if(WKQ0.length<=4){
+        //use modal
+    }
+}
