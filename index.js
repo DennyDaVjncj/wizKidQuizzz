ce2263f3188e2fac22fdee9e85e80fb4ac01c711
Denny da Vjncj <daVjncjCode@gmail.com>
2020-10-07 18:20:52 -0400

    updated function name

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-//target DOM elements for manipulation
-    //create variables to store DOM elements for targeting
-    //use variables for functioning
 
 //trigger dom 
 
@@ -8,10 +5,21 @@
 
 let WKQ=confirm("are you ready for the mental gymnastics this quiz puts one through?");//I want to create a fun, enlightning experience
 let WKQ0=prompt("here's a primer: please define the nature of reality (all things being relative of course)");
-let prmpt1Fnctn=()=>{    
-    console.log(typeof(WKQ0));
-    if(WKQ0.length<=4){
+let i;
+let usRspns=()=>{    
+    for(i=0;i<=WKQ0.length;i++){
         alert("you didn't even try though...");
+        //need to check for character length, ensure certain amount of characters are typed;
     }
 };
+usRspns();
 
+//target DOM elements for manipulation
+    //create variables to store DOM elements for targeting
+    //use variables for functioning
+
+
+//global variables
+let tQuest=[];//object for questions & answers
+let trgtDiv=document.$("#firstDiv");
+trgtDiv.html()//perhaps a mutable value
