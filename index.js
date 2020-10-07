41339adf2c954397a4160e4608ca0abb74a016db
Denny da Vjncj <daVjncjCode@gmail.com>
2020-10-07 02:09:27 -0400

    version control

--- index.js
+++ index.js
@@ -7,9 +7,11 @@
 //store questions inside array
 
 let WKQ=confirm("are you ready for the mental gymnastics this quiz puts one through?");//I want to create a fun, enlightning experience
-let WKQ0=prompt("here's a primer: please define the nature of reality (all things being relative of course)");//if input is less than 4char, alert('you didn't even try though...');
-let prmpt1Fnctn=()=>{
+let WKQ0=prompt("here's a primer: please define the nature of reality (all things being relative of course)");
+let prmpt1Fnctn=()=>{    
+    console.log(typeof(WKQ0));
     if(WKQ0.length<=4){
-        //use modal
+        alert("you didn't even try though...");
     }
-}
+};
+
