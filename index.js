99313b8b0b8fd0699cb1aa6f32b157279b626797
Denny da Vjncj <daVjncjCode@gmail.com>
2020-10-08 06:02:58 -0400

    version control

--- index.js
+++ index.js
@@ -3,14 +3,16 @@
 
 //store questions inside array
 
-let WKQ=confirm("are you ready for the mental gymnastics this quiz puts one through?");//I want to create a fun, enlightning experience
+let WKQ=confirm("are you ready for the mental gymnastics this quiz puts one through?");//I want to create a fun, enlightning experience.
 let WKQ0=prompt("here's a primer: please define the nature of reality (all things being relative of course)");
-let i;
+let i;//engine
 let usRspns=()=>{    
-    for(i=0;i<=WKQ0.length;i++){
+    for(i=4;i<=WKQ0.length;i++){
         alert("you didn't even try though...");
-        //need to check for character length, ensure certain amount of characters are typed;
+        //passedCheckpoint
     }
+
+
 };
 usRspns();
 
@@ -20,6 +22,20 @@ usRspns();
 
 
 //global variables
-let tQuest=[];//object for questions & answers
-let trgtDiv=document.$("#firstDiv");
-trgtDiv.html()//perhaps a mutable value
+let tQuest=[
+    {
+        q1:"do you know what you've gotten yourself into?",
+        q2:"what's a covert way to communicate to fellow programmers?",
+        q3:"how long will this continue",
+    },
+    {
+        a1:"doesn't seem like it",
+        a2:"with the console",
+        a3:"only time will tell"
+    }
+];//object for questions & answers//node of dynamic choices.//attach clickEvent so qstn changes after each selection, & available time is effected poitively or negatively.
+console.log(tQuest.q1);
+
+let trgtDiv=document.$("#firstDiv");//inject content to html
+// trgtDiv.html()//perhaps a mutable value, changing questions.
+ 
