c99f88ba09604c786126bdfaad9a67a00aea8a93
Denny da Vjncj <daVjncjCode@gmail.com>
2020-10-09 16:02:15 -0400

    added timer.js

--- index.js
+++ index.js
@@ -6,15 +6,15 @@
 let WKQ=confirm("are you ready for the mental gymnastics this quiz puts one through?");//I want to create a fun, enlightning experience.
 let WKQ0=prompt("here's a primer: please define the nature of reality (all things being relative of course)");
 let i;//engine
-let usRspns=()=>{    
-    for(i=4;i<=WKQ0.length;i++){
-        alert("you didn't even try though...");
-        //passedCheckpoint
+//html elements
+let DOMquests=[//will be forEach()'d through//new quest each iteration
+    {   
+        q1:"choose all that apply: what is the purpose of a function?"
     }
+]
 
 
-};
-usRspns();
+
 
 //target DOM elements for manipulation
     //create variables to store DOM elements for targeting
@@ -22,28 +22,38 @@ usRspns();
 
 
 //global variables
-let tQuest=[//iterate through objct on click, store user selection for highscores.html//forEach() to loop through object of questions
-    {
+// let tQuest=[//iterate through objct on click, store user selection for highscores.html//forEach() to loop through object of questions
+    // {
         //let crrntQst=0;//increment to track user progress
         //let=[
-        {   qstn:"lorem",
-            choices:["",],
-            answr:"i"//particular value
+        // {   qstn:"lorem",
+            // choices:["",],
+            // answr:"i"//particular value
 
-        }
-    ]
-        q1:"do you know what you've gotten yourself into?",
-        q2:"what's a covert way for communicating to fellow programmers?",
-        q3:"how long will this continue",//generates new question on evry clckEvnt
-    },
-    {
-        a1:"doesn't seem like it",//mult choice?
-        a2:"with the console",
-        a3:"only time will tell"
-    }
-];//object for questions & answers//node of dynamic choices.//attach clickEvent so qstn changes after each selection, & available time is effected poitively or negatively.
-console.log(tQuest.q1);
+//         }
+//     ]
+//         q1:"do you know what you've gotten yourself into?",
+//         q2:"what's a covert way for communicating to fellow programmers?",
+//         q3:"how long will this continue",//generates new question on evry clckEvnt
+//     },
+//     {
+//         a1:"doesn't seem like it",//mult choice?
+//         a2:"with the console",
+//         a3:"only time will tell"
+//     }
+// ];//object for questions & answers//node of dynamic choices.//attach clickEvent so qstn changes after each selection, & available time is effected poitively or negatively.
 
-let DOM=$("#firstDiv");//inject content to html
+$("#quizQuests").html("<h3>is this how I'm to question things</h3>");//inject content to html
 // trgtDiv.html()//perhaps a mutable value, changing questions.
+let DOM=$("<div>");
+
+//create object for questions
+//object for answers
+DOM.append(clusterOfQuestions);//supposition
+
+let godLike=()=>{
+    $("#quizQuests").html("<h3>is this how I'm to question things</h3>");
+}
+
+
  
